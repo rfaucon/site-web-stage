b018f44f03a26e98230c22c62980cb9333997898
rfaucon <rfaucon6@gmail.com>
2022-06-09 16:07:07 +0200

    estimation difficultés et temps dans le fichier functions.php

--- functions.php
+++ functions.php
@@ -1,92 +1,91 @@
 <?php
 
-/* Connexion à la BDD */
+/* Connexion à la BDD - 3/10 - 20 min */
 
-/* récuperation article selon son ID */
 
 
-/* on génère notre div spécifique à un produit sur la page du produit */
+/* récuperation article selon son ID - 3/10 - 15 min  */
 
 
 
-/* création du panier dans la session, si le panier n'existe pas car $_SESSION['cart'] retourne NULL alors on créé le panier */
 
+/* on génère notre div spécifique à un produit sur la page du produit - 2/10 - 15 min */
 
 
-/* on ajoute au panier en vérifiant que le panier existe, s'il existe puis si le produit est déjà présent dans le panier on augmente la quantité, sinon on ajoute le produit*/
 
+/* création du panier dans la session, si le panier n'existe pas car $_SESSION['cart'] retourne NULL alors on créé le panier - 5/10 - 45 min */
 
 
-/* affichage des éléments de la page panier */
 
+/* on ajoute au panier en vérifiant que le panier existe, s'il existe puis si le produit est déjà présent dans le panier on augmente la quantité, sinon on ajoute le produit - 6/10 - 1h*/
 
 
 
-/* modification de la quantité de l'article présent dans le panier */
+/* affichage des éléments de la page panier - 3/10 - 20 min */
 
 
 
-/* suppression d'un article dans le panier */
 
+/* modification de la quantité de l'article présent dans le panier - 4/10 - 20 min */
 
-/* calcul des frais de port */
 
 
+/* suppression d'un article dans le panier - 4/10 - 20 min*/
 
-/* affichage des frais de port */
 
+/* calcul des frais de port - 5/10 - 30 min*/
 
 
-/* calcul du prix total du panier */
 
+/* affichage des frais de port - 2/10 - 15 min */
 
 
-/* affiche le prix total du panier */ 
 
+/* calcul du prix total du panier - 3/10 - 15 min */
 
 
-/* Vide le panier */
 
+/* affiche le prix total du panier - 4/10 - 20 min */ 
 
 
-/* affiche les erreurs avec un formatage */
 
+/* Vide le panier - 3/10 - 15 min */
 
 
 
-/* restreint l'accès à une page aux personnes connectées */
+/* restreint l'accès à une page aux personnes connectées - 6/10 - 1h */
 
 
 
-/* génère une chaine aléatoire */
+/* génère une chaine aléatoire - 3/10 - 15 min*/
 
 
-/* enregistre l'adresse du client */
+/* enregistre l'adresse du client - 4/10 - 15 min */
 
 
 
 
-/* enregistre les informations de commande du client */
+/* enregistre les informations de commande du client - 5/10 - 30 min */
 
 
 
-/* enregistre les produits présents dans la commande */
+/* enregistre les produits présents dans la commande - 5/10 - 30 min*/
 
 
 
-/* actualise le stock après une commande */
+/* actualise le stock après une commande - 5/10 - 30 min */
 
 
 
 
-/* affiche le stock d'un produit */
+/* affiche le stock d'un produit 3/10 - 20 min*/
 
 
 
-/* affiche le montant des frais de port sur la page de détail d'une commande */
+/* affiche le montant des frais de port sur la page de détail d'une commande - 4/10 - 30 min*/
 
 
-/* on sauvegarde l'adresse qui a été modifiée */
+/* on sauvegarde l'adresse qui a été modifiée - 4/10 - 20 min*/
 
 
 ?>

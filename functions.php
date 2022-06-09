f99879b21d24c5df97ce6e4e21344c2096c696cd
XiberiaFR <62890250+XiberiaFR@users.noreply.github.com>
2022-06-09 15:33:10 +0200

    ajout des instructions dans le fichier functions.php

---
+++ functions.php
@@ -0,0 +1,92 @@
+<?php
+
+/* Connexion à la BDD */
+
+/* récuperation article selon son ID */
+
+
+/* on génère notre div spécifique à un produit sur la page du produit */
+
+
+
+/* création du panier dans la session, si le panier n'existe pas car $_SESSION['cart'] retourne NULL alors on créé le panier */
+
+
+
+/* on ajoute au panier en vérifiant que le panier existe, s'il existe puis si le produit est déjà présent dans le panier on augmente la quantité, sinon on ajoute le produit*/
+
+
+
+/* affichage des éléments de la page panier */
+
+
+
+
+/* modification de la quantité de l'article présent dans le panier */
+
+
+
+/* suppression d'un article dans le panier */
+
+
+/* calcul des frais de port */
+
+
+
+/* affichage des frais de port */
+
+
+
+/* calcul du prix total du panier */
+
+
+
+/* affiche le prix total du panier */ 
+
+
+
+/* Vide le panier */
+
+
+
+/* affiche les erreurs avec un formatage */
+
+
+
+
+/* restreint l'accès à une page aux personnes connectées */
+
+
+
+/* génère une chaine aléatoire */
+
+
+/* enregistre l'adresse du client */
+
+
+
+
+/* enregistre les informations de commande du client */
+
+
+
+/* enregistre les produits présents dans la commande */
+
+
+
+/* actualise le stock après une commande */
+
+
+
+
+/* affiche le stock d'un produit */
+
+
+
+/* affiche le montant des frais de port sur la page de détail d'une commande */
+
+
+/* on sauvegarde l'adresse qui a été modifiée */
+
+
+?>

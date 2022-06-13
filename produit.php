0d5138b574b0021645e441c505b4ff2a612bafac
XiberiaFR <62890250+XiberiaFR@users.noreply.github.com>
2022-06-13 17:53:03 +0200

    ajout html et css fiche produit + fonction récupération d'un produit par son id + générer div d'un produit selon son id- produit.php et functions.php

---
+++ produit.php
@@ -0,0 +1,86 @@
+<?php
+include 'db.php';
+include 'functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Hold my bear - La boutique des ours en peluche</title>
+</head>
+
+<body>
+
+    <header class="container-fluid header">
+
+
+    <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+            <div class="container collapse navbar-collapse">
+
+                <div class="col-md-3 d-flex justify-content-center">
+                    <a class="navbar-brand" href="./">Esprit moto</a>
+                </div>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#monMenu" aria-controls="monMenu" aria-label="Menu pour mobile">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="col-md-9 collapse navbar-collapse d-flex justify-content-end" id="monMenu">
+                    <ul class="navbar-nav d-flex justify-content-center align-items-center flex-row">
+
+                            <li class="p-2 nav-item"><a class="nav-link" href="compte.php">Votre compte()</a></li>
+                            <li class="p-2 nav-item"><a class="nav-link" href="deconnexion.php">Se déconnecter</a></li>
+
+                            <li class="p-2 nav-item"><a class="nav-link" href="inscription.php">Inscription</a></li>
+                            <li class="p-2 nav-item"><a class="nav-link" href="connexion.php">Connexion</a></li>
+
+                        <li class="p-2 nav-item">
+                            <a class="nav-link" href="cart.php"><i class="navigation__icon fas fa-shopping-basket"></i>Panier</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+
+        <main class="productpage" id="main">
+
+            <?php if (isset($_POST['productId'])) {
+                $productId = $_POST['productId'];
+                $product = getArticleFromID($productId);
+                singleProductPage($product);
+            }
+            ?>
+
+        </main>
+        <footer class="footer row d-flex justify-content-center text-center bg-dark mt-5 pt-3 pb-3 text-white">
+        <h3 class="footer__h3">Hold my bear, la référence des ours en peluche made in France.</h3>
+    </footer>
+    <!-- script pour le menu de bootstrap -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script>
+        $(function() {
+            var navbarNav = $("#navbarNav");
+            navbarNav.on("click", "a", null, function() {
+                navbarNav.collapse('hide');
+            });
+        });
+    </script>
+
+    <!-- Script pour chargement de fontawesome-->
+    <script src="https://kit.fontawesome.com/a4bf076c8c.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+
+</html>

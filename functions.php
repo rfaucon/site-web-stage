88b59ec76fb333a1dee105389090f9fa4e4ba3ba
XiberiaFR <62890250+XiberiaFR@users.noreply.github.com>
2022-06-14 10:24:49 +0200

    ajout code du header dans le fichier fonction avec argument pour nom de la page

--- functions.php
+++ functions.php
@@ -160,7 +160,60 @@ function creationPanier()
    return true;
 }
 
+
+
 /* on ajoute au panier en vérifiant que le panier existe, s'il existe puis si le produit est déjà présent dans le panier on augmente la quantité, sinon on ajoute le produit - 6/10 - 1h*/
+
+
+/* on ajoute le code du header */
+
+function getHeader($nomdemapage) { global $nomdusite;  ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title><?= $nomdusite . " - " . $nomdemapage;?></title>
+</head>
+
+<body>
+
+    <header class="container-fluid header">
+
+        <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+            <div class="container collapse navbar-collapse">
+
+                <div class="col-md-3 d-flex justify-content-center">
+                    <a class="navbar-brand" href="./"><?= $nomdusite ;?></a>
+                </div>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#monMenu" aria-controls="monMenu" aria-label="Menu pour mobile">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="col-md-9 collapse navbar-collapse d-flex justify-content-end" id="monMenu">
+                    <ul class="navbar-nav d-flex justify-content-center align-items-center flex-row">
+
+                            <li class="p-2 nav-item"><a class="nav-link" href="compte.php">Votre compte()</a></li>
+                            <li class="p-2 nav-item"><a class="nav-link" href="deconnexion.php">Se déconnecter</a></li>
+
+                            <li class="p-2 nav-item"><a class="nav-link" href="inscription.php">Inscription</a></li>
+                            <li class="p-2 nav-item"><a class="nav-link" href="connexion.php">Connexion</a></li>
+
+                        <li class="p-2 nav-item">
+                            <a class="nav-link" href="cart.php"><i class="navigation__icon fas fa-shopping-basket"></i>Panier</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <?php }
+
+/* on ajoute le code du footer */
 
 function footer(){?>
    <footer class="footer row d-flex justify-content-center text-center bg-dark mt-5 pt-3 pb-3 text-white">
--- index.php
+++ index.php
@@ -3,53 +3,12 @@
 include 'db.php';
 include 'functions.php';
 
+getHeader("Accueil");
 
 ?>
 
 
-<!DOCTYPE html>
-<html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-    <title><?= $nomdusite ;?> - Accueil</title>
-</head>
-
-<body>
-
-    <header class="container-fluid header">
-
-        <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
-            <div class="container collapse navbar-collapse">
-
-                <div class="col-md-3 d-flex justify-content-center">
-                    <a class="navbar-brand" href="./"><?= $nomdusite ;?></a>
-                </div>
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#monMenu" aria-controls="monMenu" aria-label="Menu pour mobile">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="col-md-9 collapse navbar-collapse d-flex justify-content-end" id="monMenu">
-                    <ul class="navbar-nav d-flex justify-content-center align-items-center flex-row">
-
-                            <li class="p-2 nav-item"><a class="nav-link" href="compte.php">Votre compte()</a></li>
-                            <li class="p-2 nav-item"><a class="nav-link" href="deconnexion.php">Se déconnecter</a></li>
-
-                            <li class="p-2 nav-item"><a class="nav-link" href="inscription.php">Inscription</a></li>
-                            <li class="p-2 nav-item"><a class="nav-link" href="connexion.php">Connexion</a></li>
-
-                        <li class="p-2 nav-item">
-                            <a class="nav-link" href="cart.php"><i class="navigation__icon fas fa-shopping-basket"></i>Panier</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         <section class="headersection vh-100 d-flex justify-content-evenly align-items-center flex-column">
             <h1 class="headersection__title">Professionnels de la moto</h1>

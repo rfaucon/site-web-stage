bfa00617bdeb3349a92353b161f79bfe80f30daf
rfaucon <rfaucon6@gmail.com>
2022-06-14 11:52:23 +0200

    ajout des commentaires du code php dans inscription.php

--- inscription.php
+++ inscription.php
@@ -4,61 +4,68 @@ include 'db.php';
 include 'functions.php';
 
 getHeader("Inscription");
-
+ // si le $_POST est différent de 
 if (!empty($_POST)) {
-
+    // opn définie la variable erreur dans un tableau 
     $errors = array();
-
+    // si le champ pseudo est vide ou contient les caractères speciaux /^[a-zA-Z0-9_]+$/ alors afficher que le pseudo contient une erreur
     if (empty($_POST['username']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_POST['username'])) {
         $errors['username'] = "Pseudo invalide, il ne peut être vide et ne peut pas contenir de caractères spéciaux";
     } else {
+        // sinon selectionner id dans la base clients et aller chercher si le pseudo n'est pas déjà utiliser 
         $query = $pdo->prepare('SELECT id FROM clients WHERE username = ?');
         $query->execute([$_POST['username']]);
         $user = $query->fetch();
         if ($user) {
+            // si le pseudo est deja connu de a base de données afficher une erreur 'Ce pseudo est déjà utilisé'
             $errors['username'] = 'Ce pseudo est déjà utilisé';
         }
     }
-
+    // si le nom de la rue ne contient pas les caractères spéciaux suivants /^[A-Za-z -éàâêèç][^0-9]{2,30}+$/ alors afficher "Nom de rue invalide" 
     if (empty($_POST['street']) || !preg_match('/^[A-Za-z -éàâêèç][^0-9]{2,30}+$/', $_POST['street'])) {
         $errors['street'] = "Nom de rue invalide";
     }
-
+    // si le numero de la rue ne contient pas les caractères spéciaux suivants /^[0-9]{1,4}+$/ alors afficher "Numero de rue invalide" 
     if (empty($_POST['number']) || !preg_match('/^[0-9]{1,4}+$/', $_POST['number'])) {
         $errors['number'] = "Numéro de rue invalide";
     }
-
+    // si le code postal ne contient pas les caractere suivant alors afficher "Code postal invalide"  
     if (empty($_POST['zipcode']) || !preg_match('/^[0-9]{5}+$/', $_POST['zipcode'])) {
         $errors['zipcode'] = "Code postal invalide";
     }
-
+    // si le nom de la ville ne contient pas les caracteres suivants alors afficher  "La ville est incorrecte"
     if (empty($_POST['city']) || !preg_match('/^[A-Za-z -éàâêèç][^0-9]{2,30}+$/', $_POST['city'])) {
         $errors['city'] = "La ville est incorrecte";
     }
-
+    // si le champ mail est vide ou n'est pas valide au format d'adress mail alors afficher "L'adresse email est incorrecte"
     if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
         $errors['email'] = "L'adresse email est incorrecte";
     } else {
+        // sinon selectionner id dans la base clients et vérifier si le mail n'est pas déjà rentrer dans la base de données 
         $query = $pdo->prepare('SELECT id FROM clients WHERE email = ?');
         $query->execute([$_POST['email']]);
         $user = $query->fetch();
         if ($user) {
+            // si le mail est déjà connu de la base de données alors afficher 'Cet email est déjà associé à un autre compte'
             $errors['email'] = 'Cet email est déjà associé à un autre compte';
         }
     }
-
+    // si le mot de passe n'est pas le meme dans les 2 champs alors afficher "Le mot de passe de confirmation ne correspond pas au mot de passe initial"
     if (empty($_POST['password']) || $_POST['password'] != $_POST['password_confirm']) {
         $errors['password'] = "Le mot de passe de confirmation ne correspond pas au mot de passe initial";
     }
 
     if (empty($errors)) {
+        // insérer dans la base clients, le pseudo, nom, prenom, email, mot de passe si ces champs ne sont pas connus de la base
         $query = $pdo->prepare("INSERT INTO clients SET username = ?, nom = ?, prenom = ?, email = ?, mot_de_passe = ?");
         $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
         $query->execute([$_POST['username'], $_POST['family_name'], $_POST['first_name'], $_POST['email'], $password]);
         $clientId = $pdo->lastInsertId();
+        // insérer dans la base adresse, idclient, adresse, code postal, ville si les informatiosn ne sont pas déjà connu de la base avec ce client en particulier
         $query = $pdo->prepare("INSERT INTO adresses SET id_client = ?, adresse = ?, code_postal = ?, ville = ?");
         $adresse = $_POST['number'] . " " . $_POST['street'];
         $query->execute([$clientId, $adresse, $_POST['zipcode'], $_POST['city']]);
+        // afficher 'Compte créé avec succès'
         die('Compte créé avec succès');
     }
 }
@@ -71,19 +78,19 @@ if (!empty($_POST)) {
 
 <main class="mt-5 pt-5 container d-flex flex-column align-items-center justify-content-center">
         <h1>S'inscrire</h1>
+        <!-- on vient vérifier si le tableau contient des erreurs,est ce que le tableau errors est différent de vide ? on vient vérifier si la variable erreur contient des erreurs -->
         <?php if (!empty($errors)) : ?>
-
-            <!-- si tous les champs du formulaire ne sont pas remplis afficher que le formlaire contient des erreurs -->
-
             <div class="alert alert-danger">
                 <p>Le formulaire contient des erreurs.</p>
                 <ul>
                     <?php foreach ($errors as $error) : ?>
                         <li><?php echo $error; ?></li>
+                    <!-- on a une boucle qui vient afficher le tableau erreurs et qui affiche les erreurs une par une -->
                     <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
             </div>
+            <!-- on va passer les données en méthode post -->
             <form class="d-flex flex-wrap justify-content-center align-items-center" action="" method="POST">
                 <!-- champ pour remplir le Pseudo -->
                 <div class="form-group col-md-5 m-3">
@@ -137,21 +144,9 @@ if (!empty($_POST)) {
                 </div>
                 <!-- bouton pour confirmer l'inscription -->
                 <button type="submit" class="col-md-3 btn btn-primary mt-3">M'inscrire</button>
-
+                <!-- c'est un bouton submit qu'on va utiliser avec la méthode submit -->
             </form>
     </main>
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 <?php
